1c3ba2f039f043263b73f9a9e6331dfa2095597a
hughmorton22 <hmmorton@unomaha.edu>
2021-10-20 17:05:01 -0500

    Done up through Target Cube Hit Effect

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Bomb : Target
+{
+    
+    public override void Process(RaycastHit hit)
+    {
+        effectScript.Play(hit, hitSound, hitEffect, effectDuration);
+        Destroy(target);
+    }
+}
